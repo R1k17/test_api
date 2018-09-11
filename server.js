077c9eb616dab99730600b7fb4c11b2ba0c47d1b
Tarik Mey <tarik-mey@gmx.de>
2018-09-10 23:52:16 -0400

    added run and close server; mlab db url added; added new get request to present the db todos

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
  const app = express();
+ const mongoose = require('mongoose');
 
- const PORT = process.env.PORT || 3000;
+ mongoose.Promise = global.Promise;
+
+//  const PORT = process.env.PORT || 3000;
+const {PORT, DATABASE_URL} = require('./config');
+const {ToDo} = require('./models');
 
  app.use(express.json());
 
@@ -9,10 +14,62 @@ const express = require('express');
   res.send('Hello World!');
 });
 
+app.get('/todos', function (req, res) {
+  ToDo.find()
+  .then(todos => {
+    res.json({
+      todos: todos.map(todo => todo.name)
+    })
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({message: "Internal server error"});
+  })
+});
+
 //  app.get('/api/*', (req, res) => {
 //    res.json({ok: true});
 //  });
 
- app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+let server;
 
- module.exports = {app};
+function runServer(databaseUrl, port=PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log("Closing server");
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+// `closeServer` function is here in original code
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+};
+
+
+ module.exports = { app, runServer, closeServer };
